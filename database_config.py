cb39324b3d3e91e0f0a0062523a7ffed697a45f7
Kwode <obruchekwode@gmail.com>
2026-05-20 22:05:53 +0100

    Fix postgres connection

--- database_config.py
+++ database_config.py
@@ -1,14 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-Base = declarative_base()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = 'postgresql://postgres:#Girlpower4life@localhost/cvapp'
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
-engine  = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-session = sessionmaker(
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
